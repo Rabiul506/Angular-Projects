ceb66519ce05710e2b52250840a17557462772ae
Rabiul506 <rabiul4kb@gmail.com>
2024-10-12 22:59:30 +0600

    In the home components hambargar menu added and functionality added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,24 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+//  menubar 
+isMenuOpen: boolean = true;
 
+// toggleMenu(){
+//   this.isMenuOpen = !this.isMenuOpen;
+// }
+closeMenu(){
+  this.isMenuOpen = !this.isMenuOpen
+}
+openMenu(){
+  this.isMenuOpen = !this.isMenuOpen
+}
 }
